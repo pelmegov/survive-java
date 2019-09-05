3b633ea46b881e714f5e24ef9b1b39c004c1a805
pelmegov <pelmegovsa@gmail.com>
2019-09-05 20:04:52 +0300

    Added configuration for main window.

--- desktop/src/ru/pelmegov/desktop/DesktopLauncher.java
+++ desktop/src/ru/pelmegov/desktop/DesktopLauncher.java
@@ -4,9 +4,42 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.pelmegov.GdxGame;
 
+import javax.swing.*;
+
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GdxGame(), config);
-	}
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = configureApp();
+
+        try {
+            new LwjglApplication(new GdxGame(), config);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, prepareErrorMessage(e));
+            System.exit(0);
+        }
+    }
+
+    private static LwjglApplicationConfiguration configureApp() {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Survive Java";
+        config.width = 1024;
+        config.height = 680;
+
+        config.backgroundFPS = 120;
+        config.foregroundFPS = 120;
+        config.vSyncEnabled = true; // Warning: affects game world update rate
+        return config;
+    }
+
+    private static String prepareErrorMessage(Exception e) {
+        StringBuilder errorMessage = new StringBuilder("Serious error occurred and the execution can not continue." + "\n\n");
+        errorMessage.append("Error information" + ": " + "\n");
+        errorMessage.append(e.toString()).append("\n");
+        errorMessage.append("Call stack" + ": " + "\n");
+        for (StackTraceElement element : e.getStackTrace()) {
+            errorMessage.append(element.toString()).append("\n");
+        }
+        return errorMessage.toString();
+    }
+
 }
